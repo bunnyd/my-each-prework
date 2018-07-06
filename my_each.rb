faceb34fcbe5276bd1b7d1495eafffff87b711a1
github username <github email address>
2018-07-05 19:21:49 -0500

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,9 @@
-def my_each(hello)
-  if block_given?
+def my_each(collection)
     counter = 0
 
-    while counter < hello.length
-      yield(hello[counter])
+    while counter < collection.length
+      yield(collection[counter])
       counter += 1
     end
-    hello
-  else
-     "Hey! No block was given!"
-  end
+    collection
 end
